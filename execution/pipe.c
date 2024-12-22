87b925dec37de5a6251a6d715ac78a5ac7f200f2
Ahmad Ben Hssain <aben-hss@e1r8p2.1337.ma>
2024-12-22 06:26:39 +0100

    pipe.c norm: OK

--- execution/pipe.c
+++ execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: aben-hss <aben-hss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 10:37:29 by orezkell          #+#    #+#             */
-/*   Updated: 2024/12/21 23:26:51 by aben-hss         ###   ########.fr       */
+/*   Updated: 2024/12/22 06:24:34 by aben-hss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,11 +71,7 @@ int	execute_r_child(t_tree *cmd, int *pipe_fd, t_env *env)
 
 void	pipe_exec(t_tree *node, t_env **env)
 {
-	int		l_child;
-	int		r_child;
-	int		pipe_fd[2];
-	int		status;
-
+	int (l_child), (r_child), (pipe_fd[2]), (status);
 	if (pipe(pipe_fd) < 0)
 		return ;
 	signal(SIGINT, SIG_IGN);
@@ -97,8 +93,6 @@ void	pipe_exec(t_tree *node, t_env **env)
 		status = (128 + WTERMSIG(status));
 	}
 	else
-		status =  (WEXITSTATUS(status));
+		status = (WEXITSTATUS(status));
 	exit_status(SET, status);
-	// exit_status(SET, ft_wait(l_child, r_child, &status));
 }
-
